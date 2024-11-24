329b1ce6dc6454e8c5410c808dd4e4e2e44addf7
Andy <abobkof@mail.ru>
2024-11-24 23:42:17 +0600

    create: vectortests fix: vector

--- SpaceBattle.Lib/Vector.cs
+++ SpaceBattle.Lib/Vector.cs
@@ -12,6 +12,11 @@
 
         public static Vector operator +(Vector vector1, Vector vector2)
         {
+            if (vector1.coords.Length != vector2.coords.Length)
+            {
+                throw new InvalidDataException("Vectors must have the same length");
+            }
+
             Vector result = new Vector(new int[vector1.coords.Length]);
             result.coords = vector1.coords.Select((value, index) => value + vector2.coords[index]).ToArray();
             return result;
@@ -24,6 +29,11 @@
         public override int GetHashCode()
         {
             return coords.GetHashCode();
+        }
+
+        public int[] GetCoords()
+        {
+            return coords;
         }
     }
 }
