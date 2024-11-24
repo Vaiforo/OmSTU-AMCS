23cfc6a60c9d9f59b94a309f7341edfe80e7f1c1
TimeFixer <ivanozhigaylo@gmail.com>
2024-11-24 23:40:02 +0600

    fix: angle

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -1,26 +1,4 @@
 namespace SpaceBattle.Lib;
-/*public class Angle
-{
-    public int valAng { get; set; }
-    public int sepAng { get; }
-    public Angle(int valAng, int sepAng)
-    {
-        this.valAng = valAng;
-        this.sepAng = sepAng;
-    }
-    public static Angle operator +(Angle a1, Angle a2)
-    {
-        if (a1.sepAng == a2.sepAng)
-        {
-            return new Angle((a1.valAng + a2.valAng) % a1.sepAng, a1.sepAng);
-        }
-        else
-        {
-            throw new Exception();
-        }
-    }
-}*/
-
 public class Angle
 {
     public int degrees { get; set; }
