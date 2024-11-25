17d45be850a4d23869cbda6a617380c54d16d01e
Andy <abobkof@mail.ru>
2024-11-26 00:56:13 +0600

    update: stt

--- SpaceBattle.Tests/StartMovingTest.cs
+++ SpaceBattle.Tests/StartMovingTest.cs
@@ -1,16 +1,31 @@
-﻿// using Moq;
-// using SpaceBattle.Lib;
+﻿using Moq;
+using SpaceBattle.Lib;
 
-// namespace SpaceBattle.Tests;
+namespace SpaceBattle.Tests;
 
-// public class StartMovingTest
-// {
-//     [Fact]
-//     public void ObjectInitPositiveTest()
-//     {
-//         var dict = new Dictionary<string, object>{};
-//         var queue = new Mock<ISender>();
-//         var moving = new Mock<ICommand>();
-//         var command = new MoveCommand(moving);
-//     }
-// }
+public class StartMovingTest
+{
+    [Fact]
+    public void ObjectInitPositiveTest()
+    {
+        var dict = new Dictionary<string, object>{};
+        var queue = new Mock<ISender>();
+        var command = new Mock<ICommand>();
+
+        var startMoving = new StartMoving(command.Object, dict, queue.Object);
+
+        Assert.NotNull(startMoving);
+    }
+
+    [Fact]
+    public void ObjectInitNegativeTest()
+    {
+        var dict = new Dictionary<string, object>{};
+        var queue = new Mock<ISender>();
+        var command = new Mock<ICommand>();
+
+        var startMoving = new StartMoving(command.Object, dict, queue.Object);
+
+        Assert.NotNull(startMoving);
+    }
+}
