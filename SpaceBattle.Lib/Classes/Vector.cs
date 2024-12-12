9f722f4e0e17207b73d1952e603d25969064ce82
vaiforic <77671997+Vaiforo@users.noreply.github.com>
2024-12-12 14:01:50 +0600

    add: vector imovi movec

---
+++ SpaceBattle.Lib/Classes/Vector.cs
@@ -0,0 +1,42 @@
+namespace SpaceBattle.Lib;
+
+public class Vector
+{
+    private int[] _coords;
+
+    public Vector(params int[] coords)
+    {
+        _coords = coords;
+    }
+
+    public static Vector operator +(Vector vector1, Vector vector2)
+    {
+        if (vector1._coords.Length != vector2._coords.Length)
+        {
+            throw new InvalidDataException("Vectors must have the same length");
+        }
+
+        var result = new Vector(new int[vector1._coords.Length])
+        {
+            _coords = vector1
+                ._coords.Select((value, index) => value + vector2._coords[index])
+                .ToArray(),
+        };
+        return result;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj != null && obj is Vector vector && _coords.SequenceEqual(vector._coords);
+    }
+
+    public override int GetHashCode()
+    {
+        return _coords.GetHashCode();
+    }
+
+    public int[] GetCoords()
+    {
+        return _coords;
+    }
+}
