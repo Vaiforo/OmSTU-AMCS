8d33bcffcaaee171982379da6fc3624c1b3c64a7
mnolimp <mnolimp@mail.ru>
2024-12-16 22:23:54 +0600

    create: registration of IoC dependency send command

---
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencySendCommand.cs
@@ -0,0 +1,16 @@
+using App;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencySendCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Commands.Send",
+            (object[] args) =>
+                new SendCommand(Ioc.Resolve<ICommand>("Adapters.ICommand", args[0]), Ioc.Resolve<ICommandReciever>("Adapters.ICommandReciever", args[1]))
+        );
+    }
+}
