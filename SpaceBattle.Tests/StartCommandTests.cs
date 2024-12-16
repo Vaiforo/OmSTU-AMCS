c088d3a2957e10d515393ec933c06593cb88e6aa
Andy <abobkof@mail.ru>
2024-12-16 22:05:11 +0600

    fix

--- SpaceBattle.Tests/StartCommandTests.cs
+++ SpaceBattle.Tests/StartCommandTests.cs
@@ -1,7 +1,19 @@
+// using Moq;
 // using SpaceBattle.Lib;
 
 // namespace SpaceBattle.Tests;
 
-// public class StartCommand {
+// public class StartCommandTest
+// {
+//     [Fact]
+//     public void StartCommandInitPositiveTest()
+//     {
+//         var cmd = new Mock<ICommand>();
+//         var dict = new Dictionary<string, object>();
+//         var queue = new Mock<ISender>();
 
+//         var startCommand = new StartCommand(cmd.Object, dict, queue.Object);
+
+//         s
+//     }
 // }
