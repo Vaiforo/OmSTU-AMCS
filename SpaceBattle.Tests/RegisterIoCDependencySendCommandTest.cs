75bc55353b587f6f1717f770a651dee843996e34
mnolimp <mnolimp@mail.ru>
2024-12-21 22:52:34 +0600

    add: resolve test for register ioc dependency send command

--- SpaceBattle.Tests/RegisterIoCDependencySendCommandTest.cs
+++ SpaceBattle.Tests/RegisterIoCDependencySendCommandTest.cs
@@ -9,11 +9,31 @@ public class RegisterIoCDependencySendCommandTests
 {
     public RegisterIoCDependencySendCommandTests()
     {
-        new InitCommand().Execute();
+        /*new InitCommand().Execute();
         var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
-        Ioc.Resolve<App.ICommand>("IoC.Scopes.Current.Set", iocScope).Execute();
+        Ioc.Resolve<App.ICommand>("IoC.Scopes.Current.Set", iocScope).Execute();*/
     }
 
     [Fact]
-    public void RegisterIoCDependencySendCommandResolvePositiveTest() { }
+    public void RegisterIoCDependencySendCommandResolvePositiveTest()
+    {
+        var commandMock = new Mock<Lib.ICommand>();
+        var messageReceiverMock = new Mock<ICommandReciever>();
+
+        var command = commandMock.Object;
+        var messageReceiver = messageReceiverMock.Object;
+
+        var registerIoCDependencySendCommand = new RegisterIoCDependencySendCommand();
+        registerIoCDependencySendCommand.Execute();
+
+        var resolveIoCDependencySendCommand = Ioc.Resolve<App.ICommand>(
+            "Commands.Send",
+            new object[] { command, messageReceiver }
+        );
+
+        Assert.NotNull(resolveIoCDependencySendCommand);
+        Assert.IsType<SendCommand>(resolveIoCDependencySendCommand);
+        resolveIoCDependencySendCommand.Execute();
+        messageReceiverMock.Verify(reciever => reciever.Recieve(command), Times.Once);
+    }
 }
