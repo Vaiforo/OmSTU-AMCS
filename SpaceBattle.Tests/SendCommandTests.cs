17433472d85b3320f103612cda6284b3832cd377
mnolimp <mnolimp@mail.ru>
2024-12-21 20:59:01 +0600

    add: tests for sending command to reciever(positive and cannot recieve command)

--- SpaceBattle.Tests/SendCommandTests.cs
+++ SpaceBattle.Tests/SendCommandTests.cs
@@ -23,8 +23,26 @@ public class SendCommandTests
         var commandRecieverMock = new Mock<ICommandReciever>();
         var commandReciever = commandRecieverMock.Object;
 
-        new SendCommand(cmd, commandReciever).Execute();
+        var sendCommand = new SendCommand(cmd, commandReciever);
+        sendCommand.Execute();
 
         commandRecieverMock.Verify(reciever => reciever.Recieve(cmd), Times.Once());
+    }
+
+    [Fact]
+    public void SendCommandSendedCommandToCommandRecieverICommandRecieverCannotGetCommandNegativeTest()
+    {
+        var commandMock = new Mock<Lib.ICommand>();
+        var cmd = commandMock.Object;
+
+        var commandRecieverMock = new Mock<ICommandReciever>();
+        commandRecieverMock
+            .Setup(commandReciever => commandReciever.Recieve(cmd))
+            .Throws(() => new Exception())
+            .Verifiable();
+        var commandReciever = commandRecieverMock.Object;
+
+        var sendCommand = new SendCommand(cmd, commandReciever);
+        Assert.Throws<Exception>(() => sendCommand.Execute());
     }
 }
