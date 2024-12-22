ec8ef165978b916c138f52c0f5297a7d0623a256
mnolimp <mnolimp@mail.ru>
2024-12-23 01:30:45 +0600

    create: endCommand

--- SpaceBattle.Lib/Classes/EndCommand.cs
+++ SpaceBattle.Lib/Classes/EndCommand.cs
@@ -2,5 +2,18 @@ namespace SpaceBattle.Lib;
 
 public class EndCommand : ICommand
 {
-    public void Execute() { }
+    private readonly IDictionary<string, object> _dict;
+    private readonly string _label;
+
+    public EndCommand(IDictionary<string, object> dict, string label)
+    {
+        _dict = dict;
+        _label = label;
+    }
+
+    public void Execute()
+    {
+        var commandInjectable = (CommandInjectableCommand)_dict[_label];
+        commandInjectable.Inject(new EmptyCommand());
+    }
 }
