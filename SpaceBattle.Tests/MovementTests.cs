fa4981fed7340b6c9818df8350c8b0e7cd96308d
Andy <abobkof@mail.ru>
2024-12-22 20:32:33 +0600

    add: movetest from maksim for green ci

---
+++ SpaceBattle.Tests/MovementTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class MovementTests
+{
+    [Fact]
+    public void ObjectUpdatePositionPositiveTest()
+    {
+        var moving = new Mock<IMovingObject>();
+
+        moving.SetupGet(x => x.Position).Returns(new Vector(12, 5)).Verifiable();
+        moving.SetupGet(x => x.Velocity).Returns(new Vector(-4, 1)).Verifiable();
+
+        var cmd = new MoveCommand(moving.Object);
+        cmd.Execute();
+
+        moving.VerifySet(x => x.Position = new Vector(8, 6), Times.Once);
+        moving.VerifyAll();
+    }
+
+    [Fact]
+    public void ObjectPositionUnreadableNegativeTest()
+    {
+        var moving = new Mock<IMovingObject>();
+
+        moving.SetupGet(x => x.Position).Throws(() => new Exception()).Verifiable();
+        moving.SetupGet(x => x.Velocity).Returns(new Vector(-4, 1)).Verifiable();
+
+        var cmd = new MoveCommand(moving.Object);
+
+        Assert.Throws<Exception>(() => cmd.Execute());
+    }
+
+    [Fact]
+    public void ObjectVelocityUnreadableNegativeTest()
+    {
+        var moving = new Mock<IMovingObject>();
+
+        moving.SetupGet(x => x.Position).Returns(new Vector(12, 5)).Verifiable();
+        moving.SetupGet(x => x.Velocity).Throws(() => new Exception()).Verifiable();
+
+        var cmd = new MoveCommand(moving.Object);
+
+        Assert.Throws<Exception>(() => cmd.Execute());
+    }
+
+    [Fact]
+    public void ObjectUpdatePositionNegativeTest()
+    {
+        var moving = new Mock<IMovingObject>();
+
+        moving.SetupGet(x => x.Position).Returns(new Vector(12, 5)).Verifiable();
+        moving.SetupGet(x => x.Velocity).Returns(new Vector(-4, 1)).Verifiable();
+
+        var cmd = new MoveCommand(moving.Object);
+
+        moving
+            .SetupSet(x => x.Position = new Vector(8, 6))
+            .Throws(() => new Exception())
+            .Verifiable();
+
+        Assert.Throws<Exception>(cmd.Execute);
+        moving.VerifyAll();
+    }
+}
