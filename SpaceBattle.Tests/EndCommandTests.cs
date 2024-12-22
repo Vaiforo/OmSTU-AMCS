66de000ea7226efe913769ad685e7a21d2a03116
mnolimp <mnolimp@mail.ru>
2024-12-23 01:32:55 +0600

    create: end command tests

---
+++ SpaceBattle.Tests/EndCommandTests.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class EndCommandTests
+{
+    [Fact]
+    public void EndCommandPositiveTest()
+    {
+        var dict = new Dictionary<string, object>();
+        var command = new CommandInjectableCommand();
+
+        dict.Add("startCommand", command);
+
+        var endCommand = new EndCommand(dict, "startCommand");
+        endCommand.Execute();
+
+        Assert.True(dict.ContainsKey("startCommand"));
+        Assert.IsType<EmptyCommand>(command._command);
+    }
+
+    [Fact]
+    public void EndCommandShouldStopCommandForConstantTimeTest()
+    {
+        var dict = new Dictionary<string, object>();
+        var label = "endCommand";
+        var command = new CommandInjectableCommand();
+        dict.Add(label, command);
+
+        var endCommand = new EndCommand(dict, label);
+        var stopwatch = Stopwatch.StartNew();
+        endCommand.Execute();
+        stopwatch.Stop();
+
+        var elapsed = stopwatch.ElapsedTicks;
+        Assert.True(elapsed < 2000, $"Execute took too long: {elapsed} ticks");
+    }
+}
