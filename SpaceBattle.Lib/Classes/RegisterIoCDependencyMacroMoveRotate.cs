8e739b80120151352ef96e3d367978ab024f5de1
Deed <off.d33d@gmail.com>
2024-12-23 01:08:04 +0600

    macrofixestwo

---
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencyMacroMoveRotate.cs
@@ -0,0 +1,12 @@
+using Hwdtech;
+namespace SpaceBattle.Lib;
+
+
+public class RegisterIoCDependencyMacroMoveRotate : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Macro.Move", (object[] args) => new CreateMacroCommandStrategy("Move").Resolve(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Macro.Rotate", (object[] args) => new CreateMacroCommandStrategy("Rotate").Resolve(args)).Execute();
+    }
+}
