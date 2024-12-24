9b0b76cf1b0079a1f0ed3cd1fbf66cc0eb6d1439
mnolimp <mnolimp@mail.ru>
2024-12-24 12:21:20 +0600

    fix: changes for using ioc dependencty empty command

--- SpaceBattle.Tests/SendCommandTests.cs
+++ SpaceBattle.Tests/SendCommandTests.cs
@@ -1,22 +1,10 @@
-﻿using Hwdtech;
-using Hwdtech.Ioc;
-using Moq;
+﻿using Moq;
 using SpaceBattle.Lib;
 
 namespace SpaceBattle.Tests;
 
 public class SendCommandTests
 {
-    public SendCommandTests()
-    {
-        new InitScopeBasedIoCImplementationCommand().Execute();
-        IoC.Resolve<Hwdtech.ICommand>(
-                "Scopes.Current.Set",
-                IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
-            )
-            .Execute();
-    }
-
     [Fact]
     public void SendCommandSendedCommandToCommandRecieverPositiveTest()
     {
