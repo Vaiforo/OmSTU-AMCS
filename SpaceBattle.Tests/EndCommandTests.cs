3da9723ba6acb1ad1517004a1fcf846572d05ef9
mnolimp <mnolimp@mail.ru>
2024-12-24 11:52:13 +0600

    fix: del test for constant time and started work on positive test

--- SpaceBattle.Tests/EndCommandTests.cs
+++ SpaceBattle.Tests/EndCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Moq;
 using SpaceBattle.Lib;
 
 namespace SpaceBattle.Tests;
@@ -9,31 +10,14 @@ public class EndCommandTests
     public void EndCommandPositiveTest()
     {
         var dict = new Dictionary<string, object>();
-        var command = new CommandInjectableCommand();
+        var commandMock = new Mock<ICommandInjectable>();
+
+        var command = commandMock.Object;
 
         dict.Add("startCommand", command);
 
         var endCommand = new EndCommand(dict, "startCommand");
         endCommand.Execute();
 
-        Assert.True(dict.ContainsKey("startCommand"));
-        Assert.IsType<EmptyCommand>(command._command);
-    }
-
-    [Fact]
-    public void EndCommandShouldStopCommandForConstantTimeTest()
-    {
-        var dict = new Dictionary<string, object>();
-        var label = "endCommand";
-        var command = new CommandInjectableCommand();
-        dict.Add(label, command);
-
-        var endCommand = new EndCommand(dict, label);
-        var stopwatch = Stopwatch.StartNew();
-        endCommand.Execute();
-        stopwatch.Stop();
-
-        var elapsed = stopwatch.ElapsedTicks;
-        Assert.True(elapsed < 10000, $"Execute took too long: {elapsed} ticks");
     }
 }
