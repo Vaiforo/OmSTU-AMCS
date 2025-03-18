c9fdca1b5837ad81a9574b4cbebf77e5eb45c47b
mnolimp <mnolimp@mail.ru>
2025-03-19 00:20:13 +0600

    add: tests class

---
+++ SpaceBattle.Tests/RegisterIoCDependenciesGameObjectRepositoryTests.cs
@@ -0,0 +1,19 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterIoCDependenciesGameObjectsRepositoryTests
+{
+    public RegisterIoCDependenciesGameObjectsRepositoryTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>(
+                "Scopes.Current.Set",
+                IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+            )
+            .Execute();
+    }
+}
