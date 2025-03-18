366755f86a391adb39c59b6261ee9b32d5dcf2a6
Andy <abobkof@mail.ru>
2025-03-18 22:53:54 +0600

    add: registertorpedostarttests

---
+++ SpaceBattle.Tests/RegisterIoCDependencyTorpedoStartMoveTests.cs
@@ -0,0 +1,40 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterIoCDependencyTorpedoStartMoveTests
+{
+    public RegisterIoCDependencyTorpedoStartMoveTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>(
+                "Scopes.Current.Set",
+                IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+            )
+            .Execute();
+    }
+
+    [Fact]
+    public void ActionsStartRegisteredPositiveTest()
+    {
+        new RegisterIoCDependencyTorpedoStartMove().Execute();
+
+        var torpedo = new Mock<IMovingObject>();
+        var dict = new Dictionary<string, object>();
+        var queue = new Mock<ISender>();
+
+        IDictionary<string, object> order = new Dictionary<string, object>
+        {
+            ["Torpedo"] = torpedo.Object,
+            ["Dictionary"] = dict,
+            ["Sender"] = queue.Object,
+        };
+
+        var resolveDependency = IoC.Resolve<TorpedoStartMove>("Actions.MoveTorpedo", order);
+        Assert.NotNull(resolveDependency);
+        Assert.IsType<TorpedoStartMove>(resolveDependency);
+    }
+}
