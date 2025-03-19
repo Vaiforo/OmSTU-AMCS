8fc6871d680335a1bbc72d50ec6fe211b60fa8b7
malvia <37767648+mnolimp@users.noreply.github.com>
2025-03-19 10:25:12 +0600

    Update RegisterIoCDependencyGameObjectRepositoryGet.cs

--- SpaceBattle.Lib/Classes/RegisterIoCDependencyGameObjectRepositoryGet.cs
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencyGameObjectRepositoryGet.cs
@@ -4,10 +4,15 @@ namespace SpaceBattle.Lib;
 
 public class RegisterIoCDependencyGameObjectsRepositoryGet : ICommand
 {
+    public readonly Dictionary<string, object> gameObjectsRepository = new Dictionary<string, object>();
+
+    public RegisterIoCDependencyGameObjectsRepositoryGet(Dictionary<string, object> _gameObjectsRepository)
+    {
+        gameObjectsRepository = _gameObjectsRepository;
+    }
+
     public void Execute()
     {
-        var gameObjectsRepository = new Dictionary<string, object>();
-
         IoC.Resolve<ICommand>(
                 "IoC.Register",
                 "Game.Item.Get",
