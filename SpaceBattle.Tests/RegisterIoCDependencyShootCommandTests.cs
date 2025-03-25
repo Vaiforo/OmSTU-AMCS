bb31c51281049140e817ca30ef587a4bb251c5a8
TimeFixer <ivanozhigaylo@gmail.com>
2025-03-25 19:07:56 +0600

    add: iocdependshootcomtest change: shootcomtest

---
+++ SpaceBattle.Tests/RegisterIoCDependencyShootCommandTests.cs
@@ -0,0 +1,37 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterIoCDependencyShootCommandTests
+{
+    public RegisterIoCDependencyShootCommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<ICommand>(
+                "Scopes.Current.Set",
+                IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+            )
+            .Execute();
+    }
+
+    [Fact]
+    public void RegisterIoCDependencyShootCommandResolvePositiveTest()
+    {
+        new RegisterShootDependency().Execute();
+        var position = new Vector(new[] { 0, 0 });
+        var shootDirection = new Vector(new[] { 1, 1 });
+        var speed = 2.0;
+        var shootCommand = IoC.Resolve<ICommand>("Command.Shoot", position, shootDirection, speed);
+
+        Assert.IsType<ShootCommand>(shootCommand);
+    }
+
+    [Fact]
+    public void RegisterIoCDependencyShootCommandResolveNegativeTest()
+    {
+        Assert.ThrowsAny<Exception>(() => IoC.Resolve<ICommand>("Command.Shoot"));
+    }
+}
