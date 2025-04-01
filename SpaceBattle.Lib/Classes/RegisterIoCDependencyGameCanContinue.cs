e1035c3ab993d5c1a1b4743bdac8a12b58276cb6
Deed <off.d33d@gmail.com>
2025-04-02 04:00:14 +0600

    add: dependecy regestration for gamecancontinue

---
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencyGameCanContinue.cs
@@ -0,0 +1,29 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyGameCanContinue : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>("IoC.Register",
+        "Game.CanContinue",
+
+        (object[] args) => {
+            var elapsedTime = (long)args[0];
+            var allowedTime = IoC.Resolve<Func<int>>("Game.AllowedTime.Get")();
+            var queueCount = IoC.Resolve<Func<int>>("Game.Queue.Count")();
+
+            if (elapsedTime < allowedTime && !(queueCount == 0))
+            {
+                return () => true;
+            }
+            else
+            {
+                return () => false;
+            }
+        }
+
+        ).Execute();
+    }
+}
