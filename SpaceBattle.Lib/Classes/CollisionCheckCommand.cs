5be18fa3994d4a285f208ffacac40c41a01a96d4
Andy <abobkof@mail.ru>
2025-04-30 08:01:34 +0600

    add: collisioncheckcommand collisionservice

---
+++ SpaceBattle.Lib/Classes/CollisionCheckCommand.cs
@@ -0,0 +1,33 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CollisionCheckCommand : ICommand
+{
+    private readonly IMovingObject _obj;
+
+    public CollisionCheckCommand(IMovingObject obj)
+    {
+        _obj = obj;
+    }
+
+    public void Execute()
+    {
+        var grid = IoC.Resolve<ISpatialPartitionGrid>("Grid.Manager");
+
+        var nearby = grid.GetNearby(_obj);
+
+        foreach (var other in nearby)
+        {
+            if (other == _obj)
+            {
+                continue;
+            }
+
+            if (IoC.Resolve<bool>("Grid.CollisionDetector", $"{_obj}.{other}"))
+            {
+                Console.WriteLine($"[COLLISION] {_obj} collided with {other}");
+            }
+        }
+    }
+}
---
+++ SpaceBattle.Lib/Classes/CollisionService.cs
@@ -0,0 +1,17 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CollisionService : ICommand
+{
+    public void Execute()
+    {
+        var grid = IoC.Resolve<ISpatialPartitionGrid>("Grid.Manager");
+
+        var allObjects = grid.GetAllOccupiedCells().SelectMany(grid.GetObjectsInCell).Distinct();
+
+        var commands = allObjects.Select(obj => new CollisionCheckCommand(obj)).ToList();
+
+        new MacroCommand(commands).Execute();
+    }
+}
