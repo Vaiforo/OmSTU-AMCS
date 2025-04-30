76ccb23c6cdf8ef02e20930410e32571d73b541f
Deed <off.d33d@gmail.com>
2025-04-30 08:54:02 +0600

    add: iocdep colision check

---
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencyCollisionCheck.cs
@@ -0,0 +1,37 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyCollisionCheck : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>(
+        "IoC.Register",
+        "Collision.Check",
+        (object[] args) =>
+        {
+            object tree = IoC.Resolve<IDictionary<int, object>>("Collision.Tree");
+
+            var values = (int[])args[0];
+
+            return values.All(value =>
+            {
+                if(tree is not IDictionary<int, object> dictionary)
+                {
+                    return false;
+                }
+
+                if(!dictionary.TryGetValue(value, out var subTree))
+                {
+                    return false;
+                }
+
+                tree = subTree;
+                return true;
+            });
+
+        }
+        ).Execute();
+    }
+}
