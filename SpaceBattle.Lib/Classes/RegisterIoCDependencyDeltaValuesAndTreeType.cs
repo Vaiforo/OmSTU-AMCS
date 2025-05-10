1f7a3f6ade83b14832706612224149960f390a88
Deed <off.d33d@gmail.com>
2025-05-10 15:06:59 +0600

    fix: dvatt

--- SpaceBattle.Lib/Classes/RegisterIoCDependencyDeltaValuesAndTreeType.cs
+++ SpaceBattle.Lib/Classes/RegisterIoCDependencyDeltaValuesAndTreeType.cs
@@ -8,7 +8,7 @@ public class RegisterIoCDependencyDeltaValuesAndTreeType : ICommand
     {
         IoC.Resolve<ICommand>(
         "IoC.Register",
-        "Collision.Get.TreeType",
+        "Collision.Get.DeltaValuesAndTreeType",
         (object[] args) =>
         {
             var object1 = args[0];
@@ -36,7 +36,7 @@ public class RegisterIoCDependencyDeltaValuesAndTreeType : ICommand
                 .Zip(positionOther, (r, o) => r - o)
                 .Concat(velocityReference
                 .Zip(velocityOther, (r, o) => r - o))
-                .ToArray();
+                 .ToArray();
 
             return (object)(deltaValues, $"{referenceType}{(referenceType == type1 ? type2 : type1)}");
         }
